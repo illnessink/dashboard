7956035125bedd9555d4c293f550d86d09b36c4b
Vanessa Foerster <nessfoerster@gmail.com>
2023-02-08 22:31:14 -0500

    wip: creates Visitors

---
+++ src/components/Visitors.js
@@ -0,0 +1,11 @@
+function Visitors() {
+    return (
+        <div>
+            <p>Website Visitors</p>
+            <h4>821</h4>
+            <div className="greyBox"></div>
+        </div>
+    );
+}
+
+export default Visitors;
