362cb49b5c88983fde9797666849a689dd417f56
Vanessa Foerster <nessfoerster@gmail.com>
2023-02-08 22:26:29 -0500

    wip: adds Info boxes to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Sidebar from './components/Sidebar'
+import Sidebar from './components/Sidebar';
+import Info from './components/Info';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Sidebar />
+      <Info whichTitle="Reviews" statOne={1281} />
+      <Info whichTitle="Average Rating" statOne={4.6} />
+      <Info whichTitle="Sentiment Analysis" statOne={960} statTwo={122} statThree={321} />
+      
     </div>
   );
 }
