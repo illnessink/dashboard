ef2926add1955b117c0d722f7fa0661ceb748aed
Vanessa Foerster <nessfoerster@gmail.com>
2023-02-08 22:31:46 -0500

    wip: adds visitors to App

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Sidebar from './components/Sidebar';
 import Info from './components/Info';
+import Visitors from './components/Visitors';
 import './App.css';
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <Info whichTitle="Reviews" statOne={1281} />
       <Info whichTitle="Average Rating" statOne={4.6} />
       <Info whichTitle="Sentiment Analysis" statOne={960} statTwo={122} statThree={321} />
-      
+      <Visitors />
     </div>
   );
 }
